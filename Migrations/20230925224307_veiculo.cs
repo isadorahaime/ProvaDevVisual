7036b651549d0e9ab8704cc7b11f53a78824e1ce
Isadora <103513099+isadorahaime@users.noreply.github.com>
2023-09-25 20:28:57 -0300

    Add files via upload

---
+++ Migrations/20230925224307_veiculo.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace APIExemplo.Migrations
+{
+    /// <inheritdoc />
+    public partial class veiculo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(type: "longtext", nullable: false),
+                    Ano = table.Column<int>(type: "int", nullable: false),
+                    Placa = table.Column<string>(type: "longtext", nullable: false),
+                    Modelo = table.Column<string>(type: "longtext", nullable: false),
+                    Marca = table.Column<string>(type: "longtext", nullable: false),
+                    Cor = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+        }
+    }
+}
---
+++ Models/Veiculo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIExemplo.Models
+{
+    public class Veiculo
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public int Ano { get; set; }
+        [Required]
+        public string Placa { get; set; }
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Cor { get; set; }
+    }
+}
